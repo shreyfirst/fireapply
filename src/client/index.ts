9dc3ec1dc749e6b5cc490e214c9fe771d7face90
Shrey Gupta <shreysgupta@gmail.com>
2024-08-11 15:15:44 -0400

    won

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,28 +1,17 @@
 /**
  * This is the client-side code that uses the inferred types from the server
  */
-import {
-	createTRPCClient,
-	splitLink,
-	unstable_httpBatchStreamLink,
-	unstable_httpSubscriptionLink,
-} from "@trpc/client";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 /**
  * We only import the `AppRouter` type from the server - this is not available at runtime
  */
 import type { AppRouter } from "../server/index.js";
 
 // Initialize the tRPC client
-const trpc = createTRPCClient<AppRouter>({
+const trpc = createTRPCProxyClient<AppRouter>({
 	links: [
-		splitLink({
-			condition: (op) => op.type === "subscription",
-			true: unstable_httpSubscriptionLink({
-				url: "http://localhost:3000",
-			}),
-			false: unstable_httpBatchStreamLink({
-				url: "http://localhost:3000",
-			}),
+		httpBatchLink({
+			url: "http://localhost:3000",
 		}),
 	],
 });
@@ -33,10 +22,7 @@ const trpc = createTRPCClient<AppRouter>({
 // - hover any types below to see the inferred types
 // - Cmd/Ctrl+click on any function to jump to the definition
 // - Rename any variable and see it reflected across both frontend and backend
-
 const users = await trpc.user.list.query();
-//    ^?
-console.log("Users:", users);
 
 const createdUser = await trpc.user.create.mutate({ name: "sachinraja" });
 //    ^?
@@ -45,9 +31,3 @@ console.log("Created user:", createdUser);
 const user = await trpc.user.byId.query("1");
 //    ^?
 console.log("User 1:", user);
-
-const iterable = await trpc.examples.iterable.query();
-
-for await (const i of iterable) {
-	console.log("Iterable:", i);
-}
