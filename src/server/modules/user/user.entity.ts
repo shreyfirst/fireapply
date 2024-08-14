e9ec192fbf5d296198bc269bc62d59ecbc38f27f
pbairol <pbairol@ncsu.edu>
2024-08-13 21:55:48 -0400

    Fixed secret key

--- src/server/modules/user/user.entity.ts
+++ src/server/modules/user/user.entity.ts
@@ -1,11 +1,11 @@
-import { Entity, Property } from "@mikro-orm/postgresql"
-import { CoreEntity } from "../../lib/helpers/coreEntity"
+import { Entity, Property } from "@mikro-orm/postgresql";
+import { CoreEntity } from "../../lib/helpers/coreEntity";
 
 @Entity()
 export class User extends CoreEntity {
-	@Property({ unique: true })
-	email!: string
+  @Property({ unique: true })
+  email!: string;
 
-	@Property()
-	password!: string
+  @Property()
+  password!: string;
 }
